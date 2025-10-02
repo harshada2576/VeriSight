313d4982552c9a538b417f5df520b328ba72b73e
harshada2576 <harshada2576@gmail.com>
2025-10-02 15:38:34 +0530

    app and product

---
+++ models/product.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, Integer, String, Float
+from ..database import Base
+
+class Product(Base):
+    __tablename__ = "products"
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    category = Column(String)
+    amount = Column(Float)
