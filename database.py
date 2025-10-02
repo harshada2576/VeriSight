1c7d667622d3b8e10783310350d18f22d4e24e0a
harshada2576 <harshada2576@gmail.com>
2025-10-02 15:20:36 +0530

    path

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
 import os
-from .utils.paths import absolute_path
+from .utils.path import absolute_path
 ENV_PATH = absolute_path(".env")
 
 load_dotenv(ENV_PATH)
