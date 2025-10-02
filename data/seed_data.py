f0f3499378fb6d26b28f34289bd85ba6f4c28739
Harshada Avhad <harshada2576@gmail.com>
2025-10-02 08:38:38 +0000

    abs path and readme

--- data/seed_data.py
+++ data/seed_data.py
@@ -1,8 +1,11 @@
 import json
 from database import SessionLocal
 from models.product import Product
+from ..utils.paths import absolute_path 
 
-with open("data/seed_data.json") as f:
+JSON_PATH = absolute_path("data", "seed_data.json") 
+
+with open(JSON_PATH) as f:
     data = json.load(f)
 
 db = SessionLocal()
--- database.py
+++ database.py
@@ -3,6 +3,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
 import os
+from .utils.paths import absolute_path
+ENV_PATH = absolute_path(".env")
 
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
